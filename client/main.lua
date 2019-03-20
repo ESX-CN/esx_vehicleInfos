fe114c5f40734f719b34112d840598c8fdfc3352
麻辣戈壁 <lpj.vip@foxmail.com>
2019-03-20 19:50:18 +0800

    修复某些情况下报错的问题

--- client/main.lua
+++ client/main.lua
@@ -32,13 +32,11 @@ Citizen.CreateThread(function()
 end)
 
 function VehicleInFront() -- Check vehicle in front
-
     local pos = GetEntityCoords(PlayerPedId())
     local entityWorld = GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 6.0, 0.0)
     local rayHandle = CastRayPointToPoint(pos.x,pos.y,pos.z - 0.8,entityWorld.x,entityWorld.y,entityWorld.z,7,PlayerPedId(),0)
     local a, b, c, d, result = GetRaycastResult(rayHandle)
     return result
-
 end
 
 Citizen.CreateThread(function() -- If there are no vehicles in front of you, it close the menu.
@@ -64,108 +62,105 @@ Citizen.CreateThread(function()
             local playerPed = PlayerPedId()
 
             local vehicle = VehicleInFront()
+            
             local closecar = GetClosestVehicle(x, y, z, 6.0, 0, 71)
-
-            local healthMeca = GetVehicleEngineHealth(vehicle)
-
-            local categ = GetVehicleClass(vehicle)
 
             local plate = GetVehicleNumberPlateText(vehicle)
 
-            local model = GetEntityModel(vehicle)
-            local modelName = GetDisplayNameFromVehicleModel(model)
+            if closecar ~= nil and plate ~= nil then
 
-            if vehicle > 0 and closecar ~= nil and model ~= 0 and modelName ~= "CARNOTFOUND" then -- If there is a vehicle in front of you then
+                local healthMeca = GetVehicleEngineHealth(vehicle)
 
-                if categ == 0 then
-                    categ = _U('Compacts')
-                elseif categ == 1 then
-                    categ = _U('Sedans')
-                elseif categ == 2 then
-                    categ = _U('SUVs')
-                elseif categ == 3 then
-                    categ = _U('Coupes')
-                elseif categ == 4 then
-                    categ = _U('Muscle')
-                elseif categ == 5 then
-                    categ = _U('Sports Classics')
-                elseif categ == 6 then
-                    categ = _U('Sports')
-                elseif categ == 7 then
-                    categ = _U('Super')
-                elseif categ == 8 then
-                    categ = _U('Motorcycles')
-                elseif categ == 9 then
-                    categ = _U('Off-road')
-                elseif categ == 10 then
-                    categ = _U('Industrial')
-                elseif categ == 11 then
-                    categ = _U('Utility')
-                elseif categ == 12 then
-                    categ = _U('Vans')
-                elseif categ == 13 then
-                    categ = _U('Cycles')
-                elseif categ == 14 then
-                    categ = _U('Boats')
-                elseif categ == 15 then
-                    categ = _U('Helicopters')
-                elseif categ == 16 then
-                    categ = _U('Planes')
-                elseif categ == 17 then
-                    categ = _U('Service')
-                elseif categ == 18 then
-                    categ = _U('Emergency')
-                elseif categ == 19 then
-                    categ = _U('Military')
-                elseif categ == 20 then
-                    categ = _U('Commercial')
-                else
-                    categ = _U('notfound')
-                end
+                local categ = GetVehicleClass(vehicle)
 
-                if healthMeca >= 850 then
-                    healthMeca = "<span style=\"color:#22780F;\">" .. _U('verygood') .. "</span>"
-                elseif healthMeca >= 600 then
-                    healthMeca = "<span style=\"color:#D1B606;\">" .. _U('good') .. "</span>"
-                elseif healthMeca >= 300 then
-                    healthMeca = "<span style=\"color:#ED7F10;\">" .. _U('medium') .. "</span>"
-                else
-                    healthMeca = "<span style=\"color:#B82010;\">" .. _U('bad') .. "</span>"
-                end
+                local model = GetEntityModel(vehicle)
+                
+                local modelName = GetDisplayNameFromVehicleModel(model)
 
-                local elements = {}
+                if vehicle > 0 and  model ~= 0 and modelName ~= "CARNOTFOUND" then -- If there is a vehicle in front of you then
 
-                table.insert(elements, {label = '-- Infos --', value = nil})
-                --
-                table.insert(elements,{
-					label = '' .. _U('model') .. '<span style="color:#CC5500;">' .. modelName .. '</span>',
-					value = nil
-				})
-                table.insert(elements,{
-					label = '' .. _U('category') .. '<span style="color:#DFAF2C;">' .. categ .. '</span>',
-					value = nil
-				})
-                if plate ~= nil then
+                    if categ == 0 then
+                        categ = _U('Compacts')
+                    elseif categ == 1 then
+                        categ = _U('Sedans')
+                    elseif categ == 2 then
+                        categ = _U('SUVs')
+                    elseif categ == 3 then
+                        categ = _U('Coupes')
+                    elseif categ == 4 then
+                        categ = _U('Muscle')
+                    elseif categ == 5 then
+                        categ = _U('Sports Classics')
+                    elseif categ == 6 then
+                        categ = _U('Sports')
+                    elseif categ == 7 then
+                        categ = _U('Super')
+                    elseif categ == 8 then
+                        categ = _U('Motorcycles')
+                    elseif categ == 9 then
+                        categ = _U('Off-road')
+                    elseif categ == 10 then
+                        categ = _U('Industrial')
+                    elseif categ == 11 then
+                        categ = _U('Utility')
+                    elseif categ == 12 then
+                        categ = _U('Vans')
+                    elseif categ == 13 then
+                        categ = _U('Cycles')
+                    elseif categ == 14 then
+                        categ = _U('Boats')
+                    elseif categ == 15 then
+                        categ = _U('Helicopters')
+                    elseif categ == 16 then
+                        categ = _U('Planes')
+                    elseif categ == 17 then
+                        categ = _U('Service')
+                    elseif categ == 18 then
+                        categ = _U('Emergency')
+                    elseif categ == 19 then
+                        categ = _U('Military')
+                    elseif categ == 20 then
+                        categ = _U('Commercial')
+                    else
+                        categ = _U('notfound')
+                    end
+
+                    if healthMeca >= 850 then
+                        healthMeca = "<span style=\"color:#22780F;\">" .. _U('verygood') .. "</span>"
+                    elseif healthMeca >= 600 then
+                        healthMeca = "<span style=\"color:#D1B606;\">" .. _U('good') .. "</span>"
+                    elseif healthMeca >= 300 then
+                        healthMeca = "<span style=\"color:#ED7F10;\">" .. _U('medium') .. "</span>"
+                    else
+                        healthMeca = "<span style=\"color:#B82010;\">" .. _U('bad') .. "</span>"
+                    end
+
+                    local elements = {}
                     table.insert(elements,{
-						label = '' .. _U('plate') .. '<span style="color:#318CE7;">' .. plate .. '</span>',
-						value = nil
-					})
+                        label = '' .. _U('model') .. '<span style="color:#CC5500;">' .. modelName .. '</span>',
+                        value = nil
+                    })
+                    table.insert(elements,{
+                        label = '' .. _U('category') .. '<span style="color:#DFAF2C;">' .. categ .. '</span>',
+                        value = nil
+                    })
+                    if plate ~= nil then
+                        table.insert(elements,{
+                            label = '' .. _U('plate') .. '<span style="color:#318CE7;">' .. plate .. '</span>',
+                            value = nil
+                        })
+                    end
+                    table.insert(elements,{
+                        label = '' .. _U('vehicleState') .. ' ' .. healthMeca .. '</span>',
+                        value = nil
+                    })
+
+                    ESX.UI.Menu.Open('default',GetCurrentResourceName(),'vehicle_infos',{title = _U('title'),align = 'top-left',elements = elements},function(data, menu) end,function(data, menu) 
+                        menu.close() 
+                    end)
+
                 end
-                table.insert(elements,{
-					label = '' .. _U('vehicleState') .. ' ' .. healthMeca .. '</span>',
-					value = nil
-				})
-
-                ESX.UI.Menu.Open('default',GetCurrentResourceName(),'vehicle_infos',{
-					title = _U('title'),
-					align = 'top-left',
-					elements = elements
-				},function(data, menu) end,function(data, menu) 
-					menu.close() 
-				end)
-
             end
-
         end
 
     end
